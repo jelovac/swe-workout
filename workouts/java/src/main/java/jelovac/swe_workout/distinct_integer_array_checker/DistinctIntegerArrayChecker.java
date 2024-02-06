2804a8b24baf7b4df254168f2abc9611f4b3b527
Vladimir Jelovac <vladimir@jelovac.net>
2024-02-06 21:17:26 +0100

    feat: solving contains duplicate problem

---
+++ workouts/java/src/main/java/jelovac/swe_workout/distinct_integer_array_checker/DistinctIntegerArrayChecker.java
@@ -0,0 +1,36 @@
+package jelovac.swe_workout.distinct_integer_array_checker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DistinctIntegerArrayChecker {
+
+  public boolean checkIsDistinctUsingSortingApproach(int[] values) {
+    Arrays.sort(values);
+
+    for (int i = 0; i < (values.length - 1); i++) {
+      int value = values[i];
+      int nextValue = values[i + 1];
+      if (value == nextValue) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public boolean checkIsDistinctUsingSecondaryListApproach(int[] values) {
+
+    ArrayList<Integer> list = new ArrayList<>();
+
+    for (int value : values) {
+      if (list.contains(value)) {
+        return false;
+      }
+
+      list.add(value);
+    }
+
+    return true;
+  }
+}
---
+++ workouts/java/src/test/java/jelovac/swe_workout/distinct_integer_array_checker/DistinctIntegerArrayCheckerTest.java
@@ -0,0 +1,41 @@
+package jelovac.swe_workout.distinct_integer_array_checker;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class DistinctIntegerArrayCheckerTest {
+
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testCheckIsDistinctUsingSortingApproach(int[] values, boolean expected) {
+    DistinctIntegerArrayChecker distinctIntegerArrayChecker = new DistinctIntegerArrayChecker();
+
+    boolean actual = distinctIntegerArrayChecker.checkIsDistinctUsingSortingApproach(values);
+
+    assertEquals(expected, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testCheckIsDistinctUsingSecondaryListApproach(int[] values, boolean expected) {
+    DistinctIntegerArrayChecker distinctIntegerArrayChecker = new DistinctIntegerArrayChecker();
+
+    boolean actual = distinctIntegerArrayChecker.checkIsDistinctUsingSecondaryListApproach(values);
+
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.of(new int[] {1, 2, 3, 1}, false),
+        Arguments.of(new int[] {1}, true),
+        Arguments.of(new int[] {}, true),
+        Arguments.of(new int[] {1, 1}, false),
+        Arguments.of(new int[] {-1, 1, 0}, true),
+        Arguments.of(new int[] {-1, 0, 1, -1}, false));
+  }
+}
