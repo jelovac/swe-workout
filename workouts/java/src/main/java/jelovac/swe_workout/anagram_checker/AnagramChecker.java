5682517de0471f1c7e8e2980b99d927ab34fbaeb
Vladimir Jelovac <vladimir@jelovac.net>
2024-02-09 08:19:39 +0100

    feat: solving is anagram problem

---
+++ workouts/java/src/main/java/jelovac/swe_workout/anagram_checker/AnagramChecker.java
@@ -0,0 +1,56 @@
+package jelovac.swe_workout.anagram_checker;
+
+import static java.util.Arrays.sort;
+
+/**
+ * Definition: Anagram is a word or phrase formed by rearranging the letters of different word or
+ * phrase using all the original letters exactly once.
+ */
+public class AnagramChecker {
+  public boolean isAnagram(String subjectInput, String anagramInput) {
+
+    // Remove all spaces and special characters from both inputs
+    // and make them lowercase
+    subjectInput = this.sanitizeInput(subjectInput);
+    anagramInput = this.sanitizeInput(anagramInput);
+
+    // Compare inputs if they are same lengths
+    int subjectLength = subjectInput.length();
+    int anagramLength = anagramInput.length();
+
+    if (subjectLength != anagramLength || subjectLength == 1) {
+      return false;
+    }
+
+    // Check if the inputs are equal
+    if (subjectInput.equals(anagramInput)) {
+      return false;
+    }
+
+    // Check if anagram input uses only subject's letters and the letters are used exactly once
+    char[] subjectCharArray = subjectInput.toCharArray();
+    sort(subjectCharArray);
+
+    char[] anagramCharArray = anagramInput.toCharArray();
+    sort(anagramCharArray);
+
+    for (int i = 0; i < subjectCharArray.length; i++) {
+      if (subjectCharArray[i] != anagramCharArray[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private String sanitizeInput(String input) {
+
+    // Remove special characters
+    String output = input.replaceAll("[^a-zA-Z0-9]", "");
+
+    // make string lowercase
+    output = output.toLowerCase();
+
+    return output;
+  }
+}
---
+++ workouts/java/src/test/java/jelovac/anagram_checker/AnagramCheckerTest.java
@@ -0,0 +1,40 @@
+package jelovac.anagram_checker;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import jelovac.swe_workout.anagram_checker.AnagramChecker;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class AnagramCheckerTest {
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testIsAnagram(String subject, String anagram, boolean expected) {
+    AnagramChecker anagramChecker = new AnagramChecker();
+
+    boolean actual = anagramChecker.isAnagram(subject, anagram);
+
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.of("anagram", "nagaram", true),
+        Arguments.of("creative", "reactive", true),
+        Arguments.of("listen", "silent", true),
+        Arguments.of("Paternal", "parental", true),
+        Arguments.of("cat", "car", false),
+        Arguments.of("c", "c", false),
+        Arguments.of("c", "d", false),
+        Arguments.of("c", "dc", false),
+        Arguments.of("cd", "d", false),
+        Arguments.of("Tom Marvolo Riddle", "I am Lord Voldemort", true),
+        Arguments.of("A gentleman!", "Elegant man.", true),
+        Arguments.of("21!", "12'", true),
+        Arguments.of("21", "12", true),
+        Arguments.of("00", "00", false),
+        Arguments.of("Creative 21", "Reactive 12", true));
+  }
+}
