03ca2ec766a0ff5b136c1b7906eef5fb3b0f9cfb
Vladimir Jelovac <vladimir@jelovac.net>
2024-06-13 20:14:06 +0200

    feat: implementing binary search using recursion

---
+++ workouts/java/src/main/java/jelovac/swe_workout/algorithms/binary_search/BinarySearchSolution2.java
@@ -0,0 +1,30 @@
+package jelovac.swe_workout.algorithms.binary_search;
+
+public class BinarySearchSolution2 {
+  int search(int targetValue, int[] sortedArray) {
+    return this.recursiveBinarySearch(targetValue, sortedArray, 0, sortedArray.length - 1);
+  }
+
+  int recursiveBinarySearch(
+      int targetValue, int[] sortedArray, int searchWindowStartIndex, int searchWindowEndIndex) {
+    if (sortedArray.length == 0) {
+      return -1;
+    }
+
+    int searchWindowMiddleIndex = (searchWindowStartIndex + searchWindowEndIndex) / 2;
+    int searchWindowMiddleValue = sortedArray[searchWindowMiddleIndex];
+
+    if (targetValue < sortedArray[searchWindowStartIndex]
+        || targetValue > sortedArray[searchWindowEndIndex]) {
+      return -1;
+    } else if (targetValue == searchWindowMiddleValue) {
+      return searchWindowMiddleIndex;
+    } else if (targetValue < searchWindowMiddleValue) {
+      return this.recursiveBinarySearch(
+          targetValue, sortedArray, searchWindowStartIndex, searchWindowMiddleIndex - 1);
+    } else {
+      return this.recursiveBinarySearch(
+          targetValue, sortedArray, searchWindowMiddleIndex + 1, searchWindowEndIndex);
+    }
+  }
+}
---
+++ workouts/java/src/test/java/jelovac/swe_workout/algorithms/binary_search/BinarySearchSolution2Test.java
@@ -0,0 +1,31 @@
+package jelovac.swe_workout.algorithms.binary_search;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class BinarySearchSolution2Test {
+
+  @ParameterizedTest
+  @MethodSource("testDataProvider")
+  public void testBinarySearchSolution2(int target, int[] sortedArray, int expected) {
+    BinarySearchSolution2 binarySearchSolution2 = new BinarySearchSolution2();
+
+    int actual = binarySearchSolution2.search(target, sortedArray);
+
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments> testDataProvider() {
+    return Stream.of(
+        Arguments.of(0, new int[] {1, 2, 3, 4}, -1),
+        Arguments.of(5, new int[] {1, 2, 3, 4}, -1),
+        Arguments.of(4, new int[] {}, -1),
+        Arguments.of(1, new int[] {1, 2, 3, 4}, 0),
+        Arguments.of(4, new int[] {1, 2, 3, 4}, 3),
+        Arguments.of(-11, new int[] {-13, -11, 2, 3, 4}, 1));
+  }
+}
